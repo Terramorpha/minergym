2e39fe1601ebe9165d20525166131872c780d5f6
Vincent Tabgoa <35674787+vtaboga@users.noreply.github.com>
2026-04-13 09:24:13 -0400

    Increase max_steps from 10,000 to 200,000

--- minergym/simulation.py
+++ minergym/simulation.py
@@ -216,7 +216,7 @@ class EnergyPlusSimulation:
     n_steps: int = field(default=0, init=False)
 
     """The amount of steps before the simulation exits."""
-    max_steps: int = 10_000
+    max_steps: int = 200_000
 
     """The directory in which energyplus will write its log files."""
     log_dir: Path = Path("eplus_output")
